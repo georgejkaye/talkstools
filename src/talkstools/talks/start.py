c98e6a0cd02fc4b9725353f1c9ccd670476d4f17
George Kaye <georgejkaye@gmail.com>
2023-12-26 17:01:39 +0000

    [impl] Add functions for starting selenium

---
+++ src/talkstools/talks/start.py
@@ -0,0 +1,13 @@
+from typing import Sequence
+from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
+
+talks_endpoint = "http://talks.bham.ac.uk"
+
+def get_talks_url(route : str, options : Sequence[tuple[str,str]] = []) -> str:
+    options_string = "&".join(list(map(lambda x : f'{x[0]}={x[1]}', options)))
+    return f"{talks_endpoint}/{route}?{options_string}"
+
+def start() -> WebDriver :
+    driver = webdriver.Chrome()
+    return driver
