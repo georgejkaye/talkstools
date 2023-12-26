2eb622e1666fe0fd29d36835a4ca68abf48a1973
George Kaye <georgejkaye@gmail.com>
2023-12-26 19:28:43 +0000

    [refact] Format start file

--- src/talkstools/talks/start.py
+++ src/talkstools/talks/start.py
@@ -5,12 +5,14 @@ from selenium.webdriver.chrome.options import Options
 
 talks_endpoint = "http://talks.bham.ac.uk"
 
-def get_talks_url(route : str, options : Sequence[tuple[str,str]] = []) -> str:
-    options_string = "&".join(list(map(lambda x : f'{x[0]}={x[1]}', options)))
+
+def get_talks_url(route: str, options: Sequence[tuple[str, str]] = []) -> str:
+    options_string = "&".join(list(map(lambda x: f"{x[0]}={x[1]}", options)))
     return f"{talks_endpoint}/{route}?{options_string}"
 
-def start() -> WebDriver :
+
+def start() -> WebDriver:
     options = Options()
-    options.add_argument('--headless=new')
+    options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
     return driver
