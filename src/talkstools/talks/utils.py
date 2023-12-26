dac4ad7428fa6787ee4a86962a8fe91a9651739e
George Kaye <georgejkaye@gmail.com>
2023-12-26 17:32:21 +0000

    [impl] Add checks for if wait for element fails

--- src/talkstools/talks/utils.py
+++ src/talkstools/talks/utils.py
@@ -4,17 +4,32 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 
-def fill_box_if_not_none(driver : WebDriver, selector: str, value : str, content : Optional[str]) -> Optional[WebElement]:
+
+def fill_box_if_not_none(
+    driver: WebDriver, selector: str, value: str, content: Optional[str]
+) -> Optional[WebElement]:
     if content is not None:
         return fill_box(driver, selector, value, content)
     else:
         return None
 
-def fill_box(driver : WebDriver, selector: str, value : str, content : str) -> WebElement:
-    box = wait_and_get(driver, selector, value)
-    box.clear()
-    box.send_keys(content)
-    return box
 
-def wait_and_get(driver : WebDriver, selector : str, value : str) -> WebElement:
-    return WebDriverWait(driver, 30).until(EC.presence_of_element_located((selector, value)))
+def fill_box(driver: WebDriver, selector: str, value: str, content: str) -> WebElement:
+    box = wait_and_get(driver, selector, value)
+    if box is None:
+        raise RuntimeError("Box not found")
+    else:
+        box.clear()
+        box.send_keys(content)
+        return box
+
+
+def wait_and_get(
+    driver: WebDriver, selector: str, value: str, timeout: int = 30
+) -> Optional[WebElement]:
+    try:
+        return WebDriverWait(driver, timeout).until(
+            EC.presence_of_element_located((selector, value))
+        )
+    except Exception:
+        return None
