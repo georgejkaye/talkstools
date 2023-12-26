eca8b6322ef07ea940d445a143b1e2128d9bbbac
George Kaye <georgejkaye@gmail.com>
2023-12-26 17:46:28 +0000

    [impl] Run chrome headless

--- src/talkstools/talks/start.py
+++ src/talkstools/talks/start.py
@@ -1,6 +1,7 @@
 from typing import Sequence
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
 
 talks_endpoint = "http://talks.bham.ac.uk"
 
@@ -9,5 +10,7 @@ def get_talks_url(route : str, options : Sequence[tuple[str,str]] = []) -> str:
     return f"{talks_endpoint}/{route}?{options_string}"
 
 def start() -> WebDriver :
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless=new')
+    driver = webdriver.Chrome(options=options)
     return driver
