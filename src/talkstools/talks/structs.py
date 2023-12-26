0859e6aee97b6c28bc0ce186f33a64af36b5cc44
George Kaye <georgejkaye@gmail.com>
2023-12-26 19:28:59 +0000

    [impl] Add string methods for talk

--- src/talkstools/talks/structs.py
+++ src/talkstools/talks/structs.py
@@ -15,3 +15,23 @@ class Talk:
     organiser_email: Optional[str] = None
     special_message: Optional[str] = None
     venue: Optional[str] = None
+
+
+def get_title_string(talk: Talk) -> str:
+    if talk.title is None:
+        return "Title to be confirmed"
+    return talk.title
+
+
+def get_speaker_string(talk: Talk) -> str:
+    if talk.speaker_name_and_affiliation is None:
+        return "Speaker to be confirmed"
+    return talk.speaker_name_and_affiliation
+
+
+def get_datetime_string(talk: Talk) -> str:
+    return f"{talk.talk_date.strftime('%d %b %Y')} {talk.talk_start.strftime('%H:%M')}-{talk.talk_end.strftime('%H:%M')}"
+
+
+def get_talk_string(talk: Talk) -> str:
+    return f"'{get_title_string(talk)}' - {get_speaker_string(talk)} ({get_datetime_string(talk)})"
