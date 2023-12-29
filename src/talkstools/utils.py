4f8b57b8f593e97188b075371f57facb0e7a39e5
George Kaye <georgejkaye@gmail.com>
2023-12-29 14:58:28 +0000

    [impl] Add default when getting env var

--- src/talkstools/utils.py
+++ src/talkstools/utils.py
@@ -1,9 +1,12 @@
 import os
+from typing import Optional
 
 
-def get_env_variable(name: str):
+def get_env_variable(name: str, default: Optional[str] = None):
     var = os.getenv(name)
     if var:
         return var
+    elif default:
+        return default
     else:
         raise ValueError(f"Environment variable {name} not set")
