433e087dd88c54e0b5f993c2637f5f4b3284e346
George Kaye <georgejkaye@gmail.com>
2023-12-30 23:28:43 +0000

    [impl] Remove selenium login route

--- src/talkstools/talks/login.py
+++ src/talkstools/talks/login.py
@@ -1,14 +1,10 @@
 from dataclasses import dataclass
 from typing import Optional
-import urllib.parse
 import requests
-from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.common.by import By
 
 from talkstools.auth import read_credentials
 
-from talkstools.talks.start import driver_get, get_talks_url
-from talkstools.talks.utils import fill_box, wait_and_get
+from talkstools.talks.start import get_talks_url
 
 login_route = "login/other_users"
 invalid_route = "login/not_raven_login"
@@ -28,53 +24,10 @@ def get_talks_credentials() -> TalksCredentials:
     return credentials
 
 
-def login_with_selenium_route(
-    endpoint: str, driver: WebDriver, credentials: Optional[TalksCredentials] = None
-) -> str:
-    print("Logging in...")
-    if credentials is None:
-        talks_credentials = get_talks_credentials()
-    else:
-        talks_credentials = credentials
-    driver_get(driver, endpoint)
-    fill_box(driver, By.ID, "email", talks_credentials.user)
-    password_box = fill_box(driver, By.ID, "password", talks_credentials.password)
-    password_box.submit()
-    element = wait_and_get(driver, By.CSS_SELECTOR, ".confirm, .error")
-    if element is None:
-        raise RuntimeError("Could not login...")
-    else:
-        elem_class = element.get_attribute("class")
-        if elem_class == "error":
-            print(element.text)
-            exit(1)
-        else:
-            print("Login successful!")
-            cookie = driver.get_cookie("_session_id")
-            if cookie is None:
-                raise RuntimeError("Could not get session cookie")
-            return cookie["value"]
-
-
-def login_with_selenium(
-    driver: WebDriver, credentials: Optional[TalksCredentials] = None
-) -> str:
-    url = get_talks_url(login_route)
-    return login_with_selenium_route(url, driver, credentials)
-
-
-def login_with_selenium_and_return(
-    driver: WebDriver, return_url: str, credentials: Optional[TalksCredentials] = None
-) -> str:
-    options = [("return_url", urllib.parse.quote(return_url, safe=""))]
-    url = get_talks_url(login_route, options=options)
-    return login_with_selenium_route(url, driver, credentials)
-
-
 login_route = "/login/not_raven_login"
 
 
-def login_with_requests(credentials: Optional[TalksCredentials] = None) -> str:
+def login(credentials: Optional[TalksCredentials] = None) -> str:
     if credentials is None:
         talks_credentials = get_talks_credentials()
     else:
@@ -93,3 +46,7 @@ def login_with_requests(credentials: Optional[TalksCredentials] = None) -> str:
     if session_id is None:
         raise RuntimeError("Could not get session id")
     return session_id
+
+
+def get_talks_session_cookies(session_id: str) -> dict[str, str]:
+    return {"_session_id": session_id}
